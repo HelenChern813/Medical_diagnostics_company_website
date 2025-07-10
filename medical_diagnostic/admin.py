ea089b10d98545338d9e986d02651c03a9d90415
Helen Chernotalova <chernotalova.e@yandex.ru>
2025-07-11 00:55:00 +0500

    Регистрация моделей в Админке Django

--- medical_diagnostic/admin.py
+++ medical_diagnostic/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Feedback
+from .models import Feedback, Services, Content, Doctors, Contacts, Appointment, DiagnosticResults
 
 
 @admin.register(Feedback)
@@ -9,3 +9,45 @@ class FeedbackAdmin(admin.ModelAdmin):
     search_fields = ('name', 'email', 'message')
     list_editable = ('is_processed',)
     date_hierarchy = 'created_at'
+
+
+@admin.register(Services)
+class ServicesAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description", "price", "photo", "doctors")
+    list_filter = ("name", "price", "doctors")
+    search_fields = ("name", "price", "doctors", "description")
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = ("id", "name_content", "text_content", "image_content")
+    list_filter = ("name_content", "text_content",)
+    search_fields = ("name_content", "text_content",)
+
+
+@admin.register(Doctors)
+class DoctorsAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "surname")
+    list_filter = ("first_name", "last_name",)
+    search_fields = ("first_name", "last_name",)
+
+
+@admin.register(Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "address")
+    list_filter = ("name", "address",)
+    search_fields = ("name", "address",)
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "appointment_date")
+    list_filter = ("user", "appointment_date",)
+    search_fields = ("user", "appointment_date",)
+
+
+@admin.register(DiagnosticResults)
+class DiagnosticResultsAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "service", "doctor", "date_performed")
+    list_filter = ("user", "service", "doctor", "date_performed",)
+    search_fields = ("user", "service", "doctor", "date_performed",)
